f699c9d229c3ed57307982e8a413dd2cf67bccb9
XiongxinXiao <xxiao079@uottawa.ca>
2019-10-01 23:27:04 -0400

    text change
    just for test

--- test1/lib/main.dart
+++ test1/lib/main.dart
@@ -92,7 +92,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have clicked the button this many times:',
+              'You have smashed the button this many times:',
             ),
             Text(
               '$_counter',
