d07a2cff0c2f534d119d2bd84fa8cc41c53a44dd
Jason Smith <jasonasmith03@gmail.com>
2019-10-02 11:12:38 -0400

    made button for landing page - no functionality

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,37 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return new Center(
-      child: Text('Landing page!!', textDirection:
-    TextDirection.ltr));
+    return new MaterialApp(
+      title: 'Autoconnect', home: new HomePage(title: 'Autoconnect')
+    );
+  }
+}
+
+class HomePage extends StatelessWidget{
+  final String title;
+
+  HomePage({this.title});
+
+  @override
+  Widget build(BuildContext context){
+    return new Scaffold(
+      appBar: new AppBar(title: new Text(this.title)),
+      body: new LandingPage());
+  }
+}
+
+class LandingPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return new Container(
+      child: Align (
+        alignment: Alignment.center,
+        child: new RaisedButton(
+          child: Text('Autoconnect'), //name of button
+      onPressed: () {} //when button is pressed it has to do something
+      ),)
+    );
+
+  
   }
 }
